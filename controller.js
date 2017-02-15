e9dd550b8e408be24a7e54bdc18074901daa4f5f
Becky Hall <uncoolplane@gmail.com>
2017-02-14 18:44:07 -0700

    example w/ notes

--- controller.js
+++ controller.js
@@ -1,10 +1,16 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	mainService.getUsers()
+    .then(function(response) {
+      console.log('controller', response);
+      $scope.users = response; //response is manipulated in the service to return data set
+      console.log('users', $scope.users);
+    }, function(error) {
+      console.err(error);
+    }); //first success, second error
   }
 
-
   $scope.getUsers();
-
+  $scope.toggleFavorite = mainService.toggleFavorite;
 });
--- service.js
+++ service.js
@@ -1,29 +1,46 @@
 angular.module('userProfiles').service('mainService', function($http) {
 
-	var data = [
-    {
-        "id": 0,
-        "first_name": "george",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
-    },
-    {
-        "id": 1,
-        "first_name": "lucille",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
-    },
-    {
-        "id": 2,
-        "first_name": "oscar",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
-    }
-  ];
+	// var data = [
+  //   {
+  //       "id": 0,
+  //       "first_name": "george",
+  //       "last_name": "bluth",
+  //       "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
+  //   },
+  //   {
+  //       "id": 1,
+  //       "first_name": "lucille",
+  //       "last_name": "bluth",
+  //       "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
+  //   },
+  //   {
+  //       "id": 2,
+  //       "first_name": "oscar",
+  //       "last_name": "bluth",
+  //       "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
+  //   }
+  // ];
 
+	var users = [];
   this.getUsers = function() {
-    return data;
+		//function being invoked, returns a promise (object with method "then")
+		return $http({
+			method: 'GET',
+			url: 'https://reqres.in/api/users?page=1'
+		}).then(function(response) { //callback function that is returning our data
+			console.log('service', response.data.data);
+			users = response.data.data;
+			return users;
+		});
+		// return data;
   };
 
+	this.toggleFavorite = function(id) {
+		users.forEach(function(user) {
+			if(user.id === id) {
+				user.isFavorite = !user.isFavorite;
+			}
 
+		})
+	}
 });
